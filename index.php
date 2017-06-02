53f5aee3acc09d0c641508318aa4487ce078047e
ClueDistro <omomen.id@gmail.com>
2017-06-02 11:08:38 +0700

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
-<html>
+
 <?php
 include "function.php";
 $random_karakter = random_karakter();
 //Membuat Cookie random_karakter yang dienkripsi dengan masa cookie 1 jam
 setcookie(base64_encode('random_karakter'), base64_encode($random_karakter), time()+3600);
 //Contoh link download yang dienkripsi
-echo "Contoh: <a href='download.php?".base64_encode("file=nama-file.zip&random=".$random_karakter)."'>nama-file.zip</a>";
+echo "Contoh: <a href='download.php?".base64_encode("file=Clue Disto Reffal.zip&random=".$random_karakter)."'>Clue Disto Reffal.zip</a>";
 echo "<br/>";
-echo "Contoh: <a href='download.php?".base64_encode("file=nama-file-lainya.zip&random=".$random_karakter)."'>nama-file-lainya.zip</a>";
+echo "Contoh: <a href='download.php?".base64_encode("file=Clue Disto Reffal.zip&random=".$random_karakter)."'>Clue Disto Reffal.zip</a>";
 exit;
 ?>
- </html>
